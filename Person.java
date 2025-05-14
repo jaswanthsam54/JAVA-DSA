a3b0dd4f5ba4643bae015d72e9cf1286b736041e
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2025-05-14 21:04:21 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {  // class name person
+    //Syntax datatype method/attribute = value;
+    int age = 20 ;
+    public static void main (String[] args)
+    {
+        // syntax ClassName Object = new Classname ;
+        Person Jashu = new Person() ; 
+        //Creating an object og class 
+        // ObjectName.Attribute/method
+        System.out.println(Jashu.age); 
+    }
+}
