c1f4b887653621fe84f01cd91dc2cb2f0bc3d22b
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2025-05-16 18:58:03 +0530

    Add files via upload

---
+++ unary.java
@@ -0,0 +1,15 @@
+public class unary
+{
+    public static void main (String[] args)
+    {
+        int a = 10;
+        int b = a++;
+        System.out.println("a=" +a);
+        System.out.println("b=" +b);
+        int c = 10;
+        int d = ++c;
+        System.out.println("c="+c);
+        System.out.println("d=" +d);
+
+    }
+}
